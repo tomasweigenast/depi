8494d9d151322e9574e2059e033576259fee365e
Tomás Weigenast <tomaswegenast@gmail.com>
2024-03-30 12:10:10 -0300

    add more docs

--- depi/lib/depi.dart
+++ depi/lib/depi.dart
@@ -1,3 +1,6 @@
+/// Depi is another dependency injection library that aims to match the functionality of .NET Dependency Injection.
+///
+/// It provides the Options pattern so you can configure your services using classes.
 library;
 
 export 'src/depi_container.dart';
